8ce98f31010e106123129977604a4806976a4b1f
Mac User <macuser@Mac-no-MacBook-Air-4.local>
2017-01-19 14:27:05 +0900

    fix bug

--- tunkCamera.py
+++ tunkCamera.py
@@ -2,13 +2,15 @@ import pygame.mixer
 import cv2
 import RPi.GPIO as GPIO
 import time
+import os
 
 forwardPinR = 27
 forwardPinL = 23
 backPinR = 17
 backPinL = 24
 
-cascade_path = "/home/pi/Downloads/tunkCamera_ver.2.0/xml/cascade.xml"
+projectPath = os.path.abspath(os.path.dirname(__file__))
+cascade_path = projectPath + "/xml/cascade.xml"
 cascade = cv2.CascadeClassifier(cascade_path)
 color = (255, 255, 255)
 cap = cv2.VideoCapture(0)
@@ -26,7 +28,7 @@ def display(frame):
 
 def playMusic():
     pygame.mixer.init()
-    pygame.mixer.music.load("/home/pi/Downloads/tunkCamera_ver.2.0/music/dog.mp3")
+    pygame.mixer.music.load(projectPath + "/music/dog.mp3")
     pygame.mixer.music.play(1)
     time.sleep(3)
     pygame.mixer.music.stop()
@@ -45,15 +47,9 @@ def goLeft():
     GPIO.output(forwardPinL, False)
     GPIO.output(backPinR, False)
 
-setupGPIO()
-while True:
-    left = 0
-    right = 0
-    lastRect = []
-    ret,frame = cap.read()
-    image_gray = cv2.cvtColor(frame, cv2.cv.CV_BGR2GRAY)
-    rect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=3, minSize=(minsize,minsize))
-
+def colorDetect(rect,frame):
+    detectedRect = []
+    
     for colorRect in rect:
         cx = colorRect[0] + (colorRect[2] / 2)
         cy = colorRect[1] + (colorRect[2] / 2)
@@ -64,26 +60,42 @@ while True:
         v = frameHSV[cy][cx][2]
 
         if h >= 15 and s >= 50 and s <= 255 and v >= 50 and v <= 255:
-            lastRect.append([colorRect[0],colorRect[1],colorRect[2],colorRect[3]])
+            detectedRect.append([colorRect[0],colorRect[1],colorRect[2],colorRect[3]])
 
-    if len(lastRect) > 0:
-        for handRect in lastRect:
-            cv2.rectangle(frame, tuple((handRect[0],handRect[1])),tuple((handRect[0]+handRect[2],handRect[1]+handRect[2])), color, thickness=2)
-            center = handRect[0] + (handRect[2] / 2)
+    return detectedRect
 
-            if center > frame.shape[1] / 2:
-                right += 1
-            else:
-                left += 1
+def detection():
+    while True:
+        left = 0
+        right = 0
+        lastRect = []
+        ret,frame = cap.read()
+        image_gray = cv2.cvtColor(frame, cv2.cv.CV_BGR2GRAY)
+        rect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=3, minSize=(minsize,minsize))
 
-    if left > right:
-	print("left")
-        goLeft()
-	playMusic()
-    elif left < right:
-	print("right")
-        goRight()
-        playMusic()
+        lastRect = colorDetect(rect,frame)
 
-    display(frame)        
-    cv2.waitKey(1)
+        if len(lastRect) > 0:
+            for handRect in lastRect:
+                cv2.rectangle(frame, tuple((handRect[0],handRect[1])),tuple((handRect[0]+handRect[2],handRect[1]+handRect[2])), color, thickness=2)
+                center = handRect[0] + (handRect[2] / 2)
+
+                if center > frame.shape[1] / 2:
+                    left += 1
+                else:
+                    right += 1
+
+        if left > right:
+	    print("left")
+            goLeft()
+	    playMusic()
+        elif left < right:
+	    print("right")
+            goRight()
+            playMusic()
+
+        display(frame)        
+        cv2.waitKey(1)
+
+setupGPIO()
+detection()
